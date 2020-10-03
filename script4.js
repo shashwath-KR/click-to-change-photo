26ba952f817ac438f9bcc106dc8d74f4f96b0344
shashwath kr <68803494+shashwath-KR@users.noreply.github.com>
2020-10-03 18:21:36 +0530

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,27 @@
+var lights= document.querySelector("#light");
+var images= document.getElementsByClassName("image1");
+
+lights.addEventListener("click", function(e) {
+e.target.classList.add("image2");
+lights.addEventListener("click", function(e) {
+e.target.classList.add("image3");
+lights.addEventListener("click", function(e) {
+e.target.classList.add("image4");
+lights.addEventListener("click", function(e) {
+e.target.classList.add("image5");
+lights.addEventListener("click", function(e) {
+e.target.classList.add("image6");
+});
+});
+});
+});
+	});
+
+
+
+	
+
+
+
+
+
